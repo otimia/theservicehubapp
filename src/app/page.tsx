fac7afb3e4437569e5f5a8e0eeaebc993c0546ca
Tellydewolf <admin@softhq.nl>
2024-10-17 00:24:02 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { SignOutButton } from "@clerk/next"
 export default function Home() {
-  return (
+  return 
+<div>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Button asChild variant="outline">
         <Link href="/auth/sign-in">Login</Link>
@@ -12,5 +13,6 @@ export default function Home() {
 <SignOutButton />
       <Button/>
     </div>
+<div/>
   );
 }
